7660f0839098a54e01f60d6aae9aedc3fa039653
Hernan Alvarado <hernanvid123@gmail.com>
2025-09-04 18:10:41 -0500

    chore: update `create-config` package version

--- packages/create-config/src/index.ts
+++ packages/create-config/src/index.ts
@@ -9,7 +9,7 @@ const program = new Command()
 program
     .name("@halvaradop/create-config")
     .description("CLI to streamline and manage @halvaradop configuration packages efficiently.")
-    .version("0.1.0")
+    .version("0.2.0")
 
 program
     .command("eslint")
